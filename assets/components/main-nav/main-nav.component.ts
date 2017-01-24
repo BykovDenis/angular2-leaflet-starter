a6892406cfeb427781e3507195538c8c30aec3dd
Denis Bykov <dbykov@owm.io>
2017-01-24 15:13:41 +0300

    Обновление фото сотрудников и адаптированность изображений по ретину

--- assets/components/main-nav/main-nav.component.ts
+++ assets/components/main-nav/main-nav.component.ts
@@ -32,16 +32,16 @@ export class MainNavComponent {
         let node = event.target.parentNode;
         let element = document.getElementById(node.id);
         if(element) {
-            var menuVane = element.querySelector('.main-nav__sub-items');
+            const menuVane = element.querySelector('.main-nav__sub-items');
             if(node.id === 'menu-vane') {
                 if(!this.flagMenuVane) {
                     menuVane.classList.add('main-nav__sub-items--enable');
-                    var icon = element.querySelector('.icon__caret-down');
+                    let icon = element.querySelector('.icon__caret-down');
                     icon.classList.remove('icon__caret-down');
                     icon.classList.add('icon__caret-up');
                 } else {
                     menuVane.classList.remove('main-nav__sub-items--enable');
-                    var icon = element.querySelector('.icon__caret-up');
+                    let icon = element.querySelector('.icon__caret-up');
                     icon.classList.add('icon__caret-down');
                     icon.classList.remove('icon__caret-up');
                 }
@@ -50,12 +50,12 @@ export class MainNavComponent {
             if(node.id === 'menu-maps') {
                 if(!this.flagMenuMaps) {
                     menuVane.classList.add('main-nav__sub-items--enable');
-                    var icon = element.querySelector('.icon__caret-down');
+                    let icon = element.querySelector('.icon__caret-down');
                     icon.classList.remove('icon__caret-down');
                     icon.classList.add('icon__caret-up');
                 } else {
                     menuVane.classList.remove('main-nav__sub-items--enable');
-                    var icon = element.querySelector('.icon__caret-up');
+                    let icon = element.querySelector('.icon__caret-up');
                     icon.classList.add('icon__caret-down');
                     icon.classList.remove('icon__caret-up');
                 }

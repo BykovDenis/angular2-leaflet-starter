60a9b5348fa40497d901ab57e4d0488a975223e8
Denis Bykov <dbykov@owm.io>
2017-04-06 17:56:51 +0300

    Убраны лишние стили

--- assets/libs.ts
+++ assets/libs.ts
@@ -11,12 +11,12 @@ import "./js/mainpage";
 // import "../node_modules/leaflet/dist/leaflet.css";
 
 import "./styles/styles.less";
-import  "./components/main-nav/main-nav.component.less";
-import  "./components/main-slider/main-slider.component.less";
-import  "./components/features/features.component.less";
-import  "./components/subscribe/subscribe.component.less";
-import  "./components/vane-language/vane-language.component.less";
-import  "./components/main-footer/main-footer.component.less";
+//import  "./components/main-nav/main-nav.component.less";
+//import  "./components/main-slider/main-slider.component.less";
+//import  "./components/features/features.component.less";
+//import  "./components/subscribe/subscribe.component.less";
+//import  "./components/vane-language/vane-language.component.less";
+//import  "./components/main-footer/main-footer.component.less";
 import  "./components/workers/workers.component.less";
 
 // import  "./components/app/app.component.less";

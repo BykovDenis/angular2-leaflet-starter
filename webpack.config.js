1654274ea84d94b2168d68511568d6600387af89
Denis Bykov <dbykov@owm.io>
2017-05-16 16:16:49 +0300

    custom webpack builder

--- webpack.config.js
+++ webpack.config.js
@@ -47,13 +47,13 @@ module.exports = {
             { test: /\.component\.html$/, loader: 'raw' },
             { test: /(\.component|)\.less$/, loader: ExtractTextPlugin.extract('to-string!css!less')}, // loaders to preprocess CSS
             { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-            { test: /\.(svg|png|gif|jpg)$/, loader: "file?name=img/[name].[ext]" },
+            { test: /\.(svg|png|gif|jpg)$/, loader: "file?name=themes/owm/assets/vendor/owm/img/[name].[ext]" },
             // For font-awesome, created by Turbo87:
             // https://gist.github.com/Turbo87/e8e941e68308d3b40ef6
-            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=fonts/[name].[ext]" },
-            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=fonts/[name].[ext]" },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=fonts/[name].[ext]" },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=fonts/[name].[ext]" }
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=themes/owm/assets/vendor/owm/fonts/[name].[ext]" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=themes/owm/assets/vendor/owm/fonts/[name].[ext]" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=themes/owm/assets/vendor/owm/fonts/[name].[ext]" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=themes/owm/assets/vendor/owm/fonts/[name].[ext]" }
         ],
         noParse: [ path.join(__dirname, 'node_modules', 'angular2', 'bundles') ]
     },
@@ -92,7 +92,7 @@ module.exports = {
             canPrint: true
         }),
         new CopyWebpackPlugin([{
-            from: path.resolve(srcDir+'/data/', 'team.json'), to: 'data/'
+            from: path.resolve(srcDir+'/data/', 'team.json'), to: 'themes/owm/assets/vendor/owm/data/'
         }]),
     ]
 };
